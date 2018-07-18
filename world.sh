173259236a87c11f2ffecb142146a52b0111fa54
sammy00 <xiangminli@outlook.com>
2018-07-18 13:16:09 +0800

    remove the non-git container alpine

--- world.sh
+++ world.sh
@@ -16,3 +16,18 @@ if ! [[ ${world2} =~ ^.*\[doc\]$ ]]; then
 fi
 
 echo "world"
+
+
+          if ! [[ ${commitDesc} =~ ^.*\[doc\]$ ]]; then 
+            exit 0
+          fi
+          git config --global user.email ${GH_EMAIL}
+          git config --global user.name ${GH_USER}
+          echo 'making doc'
+          rm -rf build
+          mkdir build
+          echo "deply hello ${CIRCLE_SHA1}" >> build/doc.txt
+          git add -A
+          git commit -m "hello deploy"
+          git push origin master
+          echo 'done making doc' 
